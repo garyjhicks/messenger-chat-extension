61f559b8ca97082c37434d0e29a469fd00bb61b5
Gary Jinghua Hicks <h_gary@outlook.com>
2020-01-11 19:37:10 -0500

    Pls work now

--- index.js
+++ index.js
@@ -14,6 +14,49 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+app.get('/', (req, res) => {
+
+    var requestSettings = {
+        url: 'https://www.google.com/images/srpr/logo11w.png',
+        method: 'GET',
+        encoding: null
+    };
+
+    request(requestSettings, function(error, response, body) {
+        res.set('Content-Type', 'image/png');
+        res.send(body);
+    });
+
+});
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+  
+    /** UPDATE YOUR VERIFY TOKEN **/
+    const VERIFY_TOKEN = "N6EkM57NkumieMB9ibbM";
+    
+    // Parse params from the webhook verification request
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Check if a token and mode were sent
+    if (mode && token) {
+    
+      // Check the mode and token sent are correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Respond with 200 OK and challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
+
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
 
